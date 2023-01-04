fec4a775433e89a88a1d9c4a5c4f0d30cdc1dc61
test <sebastian.kuppa@web.de>
2023-01-04 13:18:30 +0800

    added main.py, cnn_utils.py and weights_init()

---
+++ cnn_utils.py
@@ -0,0 +1,13 @@
+# This file contains the convolutional neural network architectures
+import torch
+from torch import nn
+
+
+def weights_init(m: torch.Tensor) -> None:
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    if classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
